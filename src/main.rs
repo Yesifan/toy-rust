aff704f396758fc7e08f83eea9841d7b19c89e47
seth <yesifan66@gmail.com>
2021-06-29 11:01:09 +0800

    chore:使用线程池的多线程处理器

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
+mod pool;
+
 use std::fs;
-use std::thread;
 use std::io::prelude::*;
-use std::net::TcpStream;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
-  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-  for stream in listener.incoming() {
-    let stream = stream.unwrap();
-
-    thread::spawn(|| {
-        handle_connection(stream);
-    });
-  }
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let tpool = pool::ThreadPool::new(4);
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        tpool.spawn(|| {
+            handle_connection(stream);
+        });
+    }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-  let mut buffer = [0; 1024];
-  stream.read(&mut buffer).unwrap();
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-  let contents = fs::read_to_string("src/index.html")
-    .expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string("src/index.html").expect("Something went wrong reading the file");
 
-  let response = format!(
-    "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-    contents.len(),
-    contents
-  );
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
 
-  stream.write(response.as_bytes()).unwrap();
-  stream.flush().unwrap();
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
